df63f8448b847cf91686dbb19fc773e09f743f56
dhumanity <115653163+dhumanity@users.noreply.github.com>
2022-11-13 20:20:33 +0000

    Used filter instead of for-loop

--- 3-PY/forecast.henri.py
+++ 3-PY/forecast.henri.py
@@ -10,19 +10,17 @@ locations = requests.get(url).json()
 def lowerAscii(input):
 	return unidecode.unidecode(input).lower()
 
-#compare ascii versions of both user input and location list
+#filter ascii location list for user inputed location and return locationID
 while True:
 	location = lowerAscii(input("Insira uma localidade: "))
 	try:
-		for loc in locations:
-			if lowerAscii(loc["local"]) == location:
-				idLocal = loc["globalIdLocal"]
-				break
-		#extra break to get out of while loop too
-		if idLocal: break
+		loc = filter(lambda loc: lowerAscii(loc["local"]) == location, locations)
+		idLocal = list(loc)[0]["globalIdLocal"]
+		break
 	except:
 		print("ERRO: Localidade não econtrada!")
 
+# guery api for by locationID and pretty print
 query = "https://api.ipma.pt/open-data/forecast/meteorology/cities/daily/" + str(idLocal) + ".json"
 forecast = requests.get(query).json()
 print(json.dumps(forecast, indent=2))
