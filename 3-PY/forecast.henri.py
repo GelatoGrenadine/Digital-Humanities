a9d0dd67f14416d9808a7f30f3b1c7919eff6e72
dhumanity <115653163+dhumanity@users.noreply.github.com>
2022-11-21 11:55:01 +0000

    Feature added `min`/`max` w/ pretty output
    DONE:
    [✔︎] changed source of json data:
    	- former list of location IDs was bigger than forecast data
    		(423 locations vs 35 cities)
    [✔︎] add logic for stale json data
    	- if not within the same day
    [✔︎] return list of max, not just an instance
    	- implemented function `filterdByFunction`
    [✔︎] prettify output by printing city name
    	- implemented function `keyReplacement`
    
    To-DO:
    [ ] bring other outputs to a pretty format

--- 3-PY/forecast.henri.py
+++ 3-PY/forecast.henri.py
@@ -1,13 +1,25 @@
 # -*- coding: utf-8 -*-
-import sys, argparse
+import sys, itertools
 import requests, json, unidecode
+from datetime import datetime, date, timedelta
+from termcolor import colored
 
-#http fetch .json file by feeding url
-url = "https://api.ipma.pt/public-data/forecast/locations.json"
-locations = requests.get(url).json()
+#http fetch .json file by feeding url:
+#url = "https://api.ipma.pt/public-data/forecast/locations.json"
+url = "https://api.ipma.pt/open-data/distrits-islands.json"
+locations = requests.get(url).json()['data']
 
 def lowerAscii(input):
 	return unidecode.unidecode(input).lower()
+
+def keyReplacement(input, key1, json, key2):
+	t = filter(lambda x: x[key1] == input, json)
+	return  list(t)[0][key2]
+
+def filterdByFunction(lst, lstIndex, function):
+	instance = function(lst, key = lambda x: x[lstIndex])
+	instances = list(filter(lambda x: x[lstIndex] == instance[lstIndex], lst))
+	return instances
 
 n_args = len(sys.argv[1:])
 minim = False
@@ -28,8 +40,8 @@ elif  n_args == 1:
 	"    [local] [dia] - Local do qual queremos ir buscar as previsões (obrigatório)\n" +
 	"                    podendo passar um dia em específico\n" +
 	"                       	Ex.: Braga 1")) 				& exit()
-	elif "min" == arg1: minim = True;
-	elif "max" == arg1: maxim = True;
+	elif "min" == arg1: minim = True
+	elif "max" == arg1: maxim = True
 	else: location = lowerAscii(arg1)
 else:
 	location = lowerAscii(arg1)
@@ -65,41 +77,66 @@ else:
 	#alt for loc in locations: json_db.append(loc['globalIdLocal'])
 	locIds = list(map(lambda locId: locId['globalIdLocal'], locations))
 
-#try too read written file, else query
-# [ ] add logic for stale data
+#try to read written file, else query
+# [✔︎] add logic for stale data
+spinner = itertools.cycle(['|', '/', '-', '\\'])
+
 while True:
 	try:
 		f2 = open("./sources/forecasts.json")
 		forecasts = json.loads(f2.read()); f2.close()
+		dataUpdate = datetime.strptime(forecasts[0]['dataUpdate'],'%Y-%m-%dT%H:%M:%S').date()
+		if dataUpdate != date.today(): raise Exception
 		break
 	except Exception as ex:
-		print("querying data, it will take up to a minute !\n"+
-				"\tit will be cached for faster retrieval ")
+		sys.stdout.write("No cached data, querying: \n")
 		forecasts = []
+		lineUp = '\033[1A'
+		lineClear = '\x1b[2K'
 		for loc in locIds:
+			sys.stdout.write(next(spinner))   # write the next character
+			sys.stdout.flush()                # flush stdout buffer (actual character display)
+			sys.stdout.write('\b')            # erase the last written char
+
 			q = str("https://api.ipma.pt/open-data/"+
 				"forecast/meteorology/cities/daily/" + str(loc) + ".json")
 			r = requests.get(q)
 			if r.status_code == 200:
 				forecasts.append(r.json())
-		fout = open("./sources/forecasts.json", "w");
+				print(lineClear, end='')
+				print("\t" + keyReplacement(loc,"globalIdLocal", locations, "local") + "\t", end='\r')
+
+		print(lineUp,end=lineClear)
+		sys.stdout.write('\033[K')		# Clear to the end of line
+
+		fout = open("./sources/forecasts.json", "w")
 		fout.write(json.dumps(forecasts)); fout.close()
+		break
 
 # filtler for every item in list, maintain: data[tMax, tMin], globalIdLocal, dataUpdate
-tmp = []; d=0;
+tmp = [];
 for loc in forecasts:
+	d=0
 	for day in loc["data"]:
 		d+=1; tmp.append(
-							[loc["globalIdLocal"],
-							 loc["dataUpdate"],
-							 d,
+							[keyReplacement(loc["globalIdLocal"], "globalIdLocal", locations, "local"),
+							 (datetime.strptime(loc["dataUpdate"],'%Y-%m-%dT%H:%M:%S')+timedelta(days=d-1)),
+							 # d,
 							 float(day["tMax"]),
 							 float(day["tMin"])])
+		#print(tmp[-1][0],"\n\t", tmp[-1][1].strftime("%a"),  colored(tmp[-1][2], 'red'),colored(tmp[-1][3], 'blue'))
 
-if maxim: print(max(tmp, key = lambda loc: loc[3]))
-elif minim: print(min(tmp, key = lambda loc: loc[4]))
+if   maxim:
+	max_values = filterdByFunction(tmp, 2, max)
+	for i in max_values:
+		print("\t\t\t\t", i[1].strftime("%a"),  colored(i[2], 'red'),i[3], "@", i[0])
+		#print(M[0],"\n\t", M[1].strftime("%a"), colored(M[2], 'red'), M[3])
+elif minim:
+	min_values = filterdByFunction(tmp, 3, min)
+	for i in min_values:
+		print("\t\t\t\t", i[1].strftime("%a"), i[2], colored(i[3], 'blue'), "@", i[0])
 
 
 # To-DO
-# [ ] return list of max, not just an instance
-# [ ] prettify output by printing city name
+# [✔︎] return list of max, not just an instance
+# [✔︎] prettify output by printing city name
